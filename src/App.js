3a4e0a07c24802482e60f263d21a41be00242940
ermineaweb <ermineaweb@gmail.com>
2021-01-15 19:26:55 +0100

    readme

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import users from "./users";
 import "./style.css";
 
 function App() {
-  return <Users users={users} />;
+  return (
+    <div className={"app"}>
+      <Users users={users} />
+      <a href="https://github.com/ermineaweb/bookmark-hook-tutorial">Sources</a>
+      <a href="https://romainamichaud.com">My website</a>
+    </div>
+  );
 }
 
 export default App;
